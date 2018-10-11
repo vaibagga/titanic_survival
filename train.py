ebb34ec14d86a7e5d2742459adf7aea23005b4fe
ssulekhiya <44071321+ssulekhiya@users.noreply.github.com>
2018-10-12 01:16:51 +0530

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ of a passenger aboard the Titanic
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-#from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LogisticRegression :)
 from sklearn.metrics import accuracy_score
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
